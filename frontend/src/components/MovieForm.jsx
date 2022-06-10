eceac88ffb17acd54dbe5a34dd4aa68ba86dd416
JonasStempickij <86933321+JonasStempickij@users.noreply.github.com>
2022-06-10 13:21:03 +0300

    fixed double render

--- frontend/src/components/MovieForm.jsx
+++ frontend/src/components/MovieForm.jsx
@@ -1,51 +1,53 @@
-import { useDispatch } from "react-redux";
-import { setGenre } from "../features/movies/movieSlice";
+import { useDispatch } from 'react-redux';
+import { setGenre } from '../features/movies/movieSlice';
 
 const MovieForm = ({ genre }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const genres = [
-    "all",
-    "horror",
-    "action",
-    "drama",
-    "thriller",
-    "crime",
-    "mystery",
-    "music",
-    "sport",
-    "animation",
-    "adventure",
-    "family",
-    "romance",
-    "sci-fi",
-    "comedy",
-    "western",
-    "war",
-    "biography",
-    "film-noir",
-  ];
+    const genres = [
+        'all',
+        'horror',
+        'action',
+        'drama',
+        'thriller',
+        'crime',
+        'mystery',
+        'music',
+        'sport',
+        'animation',
+        'adventure',
+        'family',
+        'romance',
+        'sci-fi',
+        'comedy',
+        'western',
+        'war',
+        'biography',
+        'film-noir',
+    ];
 
-  const onChange = (e) => {
-    dispatch(setGenre(e.target.value));
-  };
+    const onChangeGenre = (e) => {
+        dispatch(setGenre(e.target.value));
+    };
 
-  return (
-    <section className="text-center mb-3">
-      <div className="form-group">
-        <p>Filter by genre: </p>
-        <select name="genre" value={genre} onChange={onChange}>
-          {genres.map((genreOption, index) => {
-            return (
-              <option key={index} value={genreOption}>
-                {genreOption}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-    </section>
-  );
+    return (
+        <section className='d-flex flex-column gap-2 mb-3 align-items-center'>
+            <div className='d-flex'>
+                <label htmlFor='genre' className='me-3'>
+                    Filter by genre:{' '}
+                </label>
+                <select name='genre' value={genre} onChange={onChangeGenre}>
+                    {genres.map((genreOption, index) => {
+                        return (
+                            <option key={index} value={genreOption}>
+                                {genreOption}
+                            </option>
+                        );
+                    })}
+                </select>
+            </div>
+        </section>
+    );
 };
 
 export default MovieForm;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
-import "./assets/css/main.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import './assets/css/main.css';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <App />
     </Provider>
-  </React.StrictMode>
 );
